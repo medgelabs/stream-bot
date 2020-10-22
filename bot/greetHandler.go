9f581e68ab9e920b48f4df72635ba637fe4dfdd5
Matthew Edge <medge@medgelabs.io>
2020-10-21 21:40:46 -0400

    Don't greet empty strings

--- bot/greetHandler.go
+++ bot/greetHandler.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"log"
 	"medgebot/irc"
+	"strings"
 	"time"
 )
 
 func (bot *Bot) RegisterGreeter(ledger Ledger) {
 	bot.RegisterHandler(func(msg irc.Message) {
 		username := msg.User
+		if strings.TrimSpace(username) == "" {
+			return
+		}
+
 		if ledger.Absent(username) {
 			log.Printf("Never seen %s before", username)
 			ledger.Add(username)
