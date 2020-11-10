2b590b683accf55ff0b6a9da0129a2c464b8f0b6
Matthew Edge <MatthewEdge@users.noreply.github.com>
2020-11-10 17:47:20 -0500

    Dynamic Feature Toggles (#4)
    * Add CLI flags to turn features on. Off by default
    * Add ranaebot and soundalerts to ignore

--- main.go
+++ main.go
@@ -16,18 +16,23 @@ import (
 
 func main() {
 
-	// channel and nick come from the CLI
+	// CLI argument processing
 	var channel string
 	var nick string
+	var ledgerType string
+	var secretStoreType string
+	var enableAll bool
+	var enableGreeter bool
+	var enableCommands bool
+
 	flag.StringVar(&channel, "channel", "", "Channel name, without the #, to join")
 	flag.StringVar(&nick, "nick", "", "Nickname to join Chat with")
-
-	// Inputs for factories
-	var ledgerType string
 	flag.StringVar(&ledgerType, "ledger", ledger.REDIS, fmt.Sprintf("Ledger type string. Options: %s, %s, %s", ledger.REDIS, ledger.FILE, ledger.MEM))
-
-	var secretStoreType string
 	flag.StringVar(&secretStoreType, "store", secret.VAULT, fmt.Sprintf("Secret Store type string. Options: %s, %s", secret.VAULT, secret.ENV))
+
+	flag.BoolVar(&enableAll, "all", false, "Enable all features")
+	flag.BoolVar(&enableGreeter, "greeter", false, "Enable the auto-greeter")
+	flag.BoolVar(&enableCommands, "commands", false, "Enable Command processing")
 
 	flag.Parse()
 
@@ -49,21 +54,56 @@ func main() {
 		log.Fatalf("FATAL: read config.yaml - %v", err)
 	}
 
-	// Ledger for the auto greeter
-	ledger, err := ledger.NewLedger(ledgerType)
-	if err != nil {
-		log.Fatalf("FATAL: create ledger - %v", err)
+	// Initialize desired state for the bot
+	chatBot := bot.New()
+	chatBot.RegisterPingPong()
+	chatBot.RegisterReadLogger()
+
+	if enableCommands || enableAll {
+		chatBot.HandleCommands()
 	}
 
-	// pre-seed names we don't want greeted
-	// TODO make variadic
-	ledger.Add("tmi.twitch.tv")
-	ledger.Add("streamlabs")
-	ledger.Add("nightbot")
-	ledger.Add(nick)
-	ledger.Add(strings.TrimPrefix(channel, "#")) // Prevent greeting the broadcaster
-	ledger.Add(nick + ".tmi.twitch.tv")
-	ledger.Add(nick + "@tmi.twitch.tv")
+	if enableGreeter || enableAll {
+		// Ledger for the auto greeter
+		ledger, err := ledger.NewLedger(ledgerType)
+		if err != nil {
+			log.Fatalf("FATAL: create ledger - %v", err)
+		}
+
+		// pre-seed names we don't want greeted
+		// TODO make variadic
+		ledger.Add("tmi.twitch.tv")
+		ledger.Add("streamlabs")
+		ledger.Add("nightbot")
+		ledger.Add("ranaebot")
+		ledger.Add("soundalerts")
+		ledger.Add(nick)
+		ledger.Add(strings.TrimPrefix(channel, "#")) // Prevent greeting the broadcaster
+		ledger.Add(nick + ".tmi.twitch.tv")
+		ledger.Add(nick + "@tmi.twitch.tv")
+
+		// Greeter config
+		var greetConfig greeter.Config
+		confKey := fmt.Sprintf("greeter.%s", strings.Trim(channel, "#"))
+		confSub := viper.Sub(confKey)
+		if confSub == nil {
+			log.Fatalf("FATAL: key %s not found in config", confKey)
+		}
+
+		confSub.Unmarshal(&greetConfig)
+		greetBot := greeter.New(greetConfig, ledger)
+		chatBot.RegisterGreeter(greetBot)
+	}
+
+	// chatBot.RegisterEmoteCounter()
+	// chatBot.RegisterRaidHander()
+	// chatbot.RegisterFollowTracker()
+	// chatbot.RegisterSubscriberTracker()
+
+	if err := chatBot.Connect(); err != nil {
+		log.Fatalf("FATAL: bot connect - %v", err)
+	}
+	defer chatBot.Close()
 
 	// Initialize Secrets Store
 	store, err := secret.NewSecretStore(secretStoreType)
@@ -75,33 +115,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("FATAL: Get Twitch Token from store - %v", err)
 	}
-
-	// Greeter config
-	var greetConfig greeter.Config
-	confKey := fmt.Sprintf("greeter.%s", strings.Trim(channel, "#"))
-	confSub := viper.Sub(confKey)
-	if confSub == nil {
-		log.Fatalf("FATAL: key %s not found in config", confKey)
-	}
-
-	confSub.Unmarshal(&greetConfig)
-	greetBot := greeter.New(greetConfig, ledger)
-
-	// Initialize desired state for the bot
-	chatBot := bot.New()
-	chatBot.RegisterPingPong()
-	chatBot.RegisterReadLogger()
-	chatBot.HandleCommands()
-	chatBot.RegisterGreeter(greetBot)
-	// chatBot.RegisterEmoteCounter()
-	// chatBot.RegisterRaidHander()
-	// chatbot.RegisterFollowTracker()
-	// chatbot.RegisterSubscriberTracker()
-
-	if err := chatBot.Connect(); err != nil {
-		log.Fatalf("FATAL: bot connect - %v", err)
-	}
-	defer chatBot.Close()
 
 	if err := chatBot.Authenticate(nick, password); err != nil {
 		log.Fatalf("FATAL: bot authentication failure - %s", err)
