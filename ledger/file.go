38c622abdb971208cde8076b9be295ea6cdb12ad
Matthew Edge <medge@medgelabs.io>
2020-12-19 08:16:12 -0500

    Add expiring keys to the File Ledger

--- ledger/file.go
+++ ledger/file.go
@@ -1,24 +1,29 @@
 package ledger
 
 import (
-	"io"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
-// TODO expiration consideration
-// Do we use fs timestamp (per shito86)? or do we need structured files like JSON with a ts?
-
+// FileLedger is a in-memory cache backed by a FS file
+// Note: the map is map[username]UNIX_EPOCH_SECONDS to account for expiring keys
 type FileLedger struct {
-	ledger    io.ReadWriter
-	cache     map[string]int
-	separator string
+	ledger         *os.File
+	cache          map[string]int64
+	lineSeparator  string
+	fieldSeparator string
+	expiration     int64
 }
 
-func NewFileLedger(ledger io.ReadWriter) (FileLedger, error) {
-	cache := make(map[string]int)
-	separator := "\n"
+func NewFileLedger(ledger *os.File, keyExpirationTime int64) (FileLedger, error) {
+	cache := make(map[string]int64)
+	lineSeparator := "\n"
+	fieldSeparator := ","
 
 	bytes, err := ioutil.ReadAll(ledger)
 	if err != nil {
@@ -28,25 +33,95 @@ func NewFileLedger(ledger io.ReadWriter) (FileLedger, error) {
 
 	// Hydrate cache from Reader
 	str := string(bytes)
-	for _, user := range strings.Split(str, separator) {
-		cache[user] = 1
+	for _, line := range strings.Split(str, lineSeparator) {
+		// skip empty lines
+		if len(line) <= 1 {
+			continue
+		}
+
+		tokens := strings.Split(line, fieldSeparator)
+
+		// Don't add invalid lines
+		if len(tokens) != 2 {
+			log.Printf("ERROR: invalid ledger line - %s", line)
+			continue
+		}
+
+		// And don't add keys that are expired
+		key := tokens[0]
+		ts, err := strconv.ParseInt(tokens[1], 10, 64)
+		if err != nil {
+			log.Printf("ERROR: invalid timestamp for key %s - %s", key, tokens[1])
+			continue
+		}
+
+		expirationTime := time.Now().Unix() - keyExpirationTime
+		if ts < expirationTime {
+			continue
+		}
+
+		// Valid entry, add to the cache
+		cache[key] = ts
 	}
 
-	return FileLedger{
-		ledger:    ledger,
-		cache:     cache,
-		separator: separator,
-	}, nil
+	// To ensure we remove stale data, we rewrite state to the ledger
+	fileLedger := FileLedger{
+		ledger:         ledger,
+		cache:          cache,
+		lineSeparator:  lineSeparator,
+		fieldSeparator: fieldSeparator,
+		expiration:     keyExpirationTime,
+	}
+
+	// Setup cache flushing
+	fileLedger.flushCache()
+	go func(ledger *FileLedger) {
+		for {
+			select {
+			case <-time.After(10 * time.Second):
+				fileLedger.flushCache()
+			}
+		}
+	}(&fileLedger)
+
+	return fileLedger, nil
+}
+
+func (l *FileLedger) flushCache() {
+	// Truncate(0) clears the file contents
+	l.ledger.Truncate(0)
+
+	for username, ts := range l.cache {
+		l.ledger.Write([]byte(l.line(username, ts)))
+	}
 }
 
 func (l *FileLedger) Absent(key string) bool {
-	_, ok := l.cache[key]
-	return !ok
+	entryTs, ok := l.cache[key]
+	return !ok || l.expired(entryTs)
 }
 
 func (l *FileLedger) Add(key string) error {
-	l.cache[key] = 1
-	l.ledger.Write([]byte(key + l.separator))
+	// Don't add a key if it already exists && is not expired
+	entryTs, ok := l.cache[key]
+	if ok || !l.expired(entryTs) {
+		return nil
+	}
+
+	ts := time.Now().Unix()
+	l.cache[key] = ts
+	l.ledger.Write([]byte(l.line(key, ts)))
 
 	return nil
+}
+
+// line returns a string representation of a formatted ledger line
+func (l *FileLedger) line(username string, timestamp int64) string {
+	return fmt.Sprintf("%s%s%d%s", username, l.fieldSeparator, timestamp, l.lineSeparator)
+}
+
+// expired checks if the given key's timestamp is beyond the expiration threshold
+func (l *FileLedger) expired(entryTs int64) bool {
+	expirationTime := time.Now().Unix() - l.expiration
+	return entryTs < expirationTime
 }
--- ledger/ledgerFactory.go
+++ ledger/ledgerFactory.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 	"strings"
-	"time"
 )
 
 // Acceptable types of Ledgers
@@ -15,12 +14,12 @@ const (
 	MEM   = "mem"
 )
 
-func NewLedger(ledgerType string) (Ledger, error) {
+func NewLedger(ledgerType string, keyExpirationTime int64) (Ledger, error) {
 	switch strings.ToLower(ledgerType) {
 	case REDIS:
 		redisHost := os.Getenv("REDIS_HOST")
 		redisPort := os.Getenv("REDIS_PORT")
-		redis, err := NewRedisLedger(redisHost, redisPort)
+		redis, err := NewRedisLedger(redisHost, redisPort, keyExpirationTime)
 		return &redis, err
 	case FILE:
 		file, err := ledgerFile("ledger.txt")
@@ -28,7 +27,7 @@ func NewLedger(ledgerType string) (Ledger, error) {
 			log.Fatalf("FATAL: load ledger file - %v", err)
 		}
 
-		ledger, err := NewFileLedger(file)
+		ledger, err := NewFileLedger(file, keyExpirationTime)
 		if err != nil {
 			log.Fatalf("FATAL: read ledger file - %v", err)
 		}
@@ -42,28 +41,7 @@ func NewLedger(ledgerType string) (Ledger, error) {
 }
 
 func ledgerFile(filepath string) (*os.File, error) {
-	stats, err := os.Stat(filepath)
-	if err != nil {
-		log.Printf("ERROR: failed to stat %s - %v", filepath, err)
-		return &os.File{}, err
-	}
-	lastModTime := stats.ModTime()
-
-	// Due to Sub()'s weird rejection of time.Duration, we add negative
-	// to get 12 hours in the past
-	twelveHoursAgo := time.Now().Add(-12 * time.Hour)
-
 	// If the file is more than 12 hours ago, clear
-	if lastModTime.Before(twelveHoursAgo) {
-		log.Println("Ledger file older than 12 hours. Clearing..")
-
-		err := os.Remove(filepath)
-		if err != nil {
-			log.Printf("ERROR: failed to remove ledger file %s - %v", filepath, err)
-			return &os.File{}, err
-		}
-	}
-
 	ledgerFile, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 	return ledgerFile, err
 }
