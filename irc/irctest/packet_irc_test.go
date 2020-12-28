5b10a88811f0bb2951ff04e883308833fa5f12b3
Matthew Edge <medge@medgelabs.io>
2020-12-28 09:17:02 -0500

    Fill out tests for packet_irc.go

--- irc/irctest/packet_irc_test.go
+++ irc/irctest/packet_irc_test.go
@@ -4,24 +4,148 @@ import (
 	"testing"
 )
 
+const (
+	user    = "medgelabs"
+	channel = "medgelabs"
+)
+
 func TestMakeRaidMessage(t *testing.T) {
-	raider := "medgelabs"
 	result := MakeRaidMessage("medgelabs", 1, "medgelabs")
 
 	if !HasTag(result, "msg-id", "raid") {
 		t.Fatalf("Missing tag msg-id. Got %s", result)
 	}
 
-	if !HasTag(result, "display-name", raider) {
+	if !HasTag(result, "display-name", user) {
 		t.Fatalf("Missing tag display-name. Got %s", result)
 	}
 
-	if !HasTag(result, "msg-param-displayName", raider) {
+	if !HasTag(result, "msg-param-displayName", user) {
 		t.Fatalf("Missing tag msg-param-displayName. Got %s", result)
 	}
 
 	if !HasTag(result, "msg-param-viewerCount", "1") {
 		t.Fatalf("Missing tag msg-param-viewerCount. Got %s", result)
+	}
+
+	if !HasCommand(result, "USERNOTICE") {
+		t.Fatalf("Missing command USERNOTICE. Got %s", result)
+	}
+}
+
+func TestMakeChatMessage(t *testing.T) {
+	result := MakeChatMessage(user, "Hello", channel)
+
+	if !HasTag(result, "display-name", user) {
+		t.Fatalf("Missing tag display-name. Got %s", result)
+	}
+
+	if !HasCommand(result, "PRIVMSG") {
+		t.Fatalf("Missing command PRIVMSG. Got %s", result)
+	}
+}
+
+func TestMakeBitsMessage(t *testing.T) {
+	result := MakeBitsMessage(user, 100, channel)
+
+	if !HasTag(result, "display-name", user) {
+		t.Fatalf("Missing tag display-name. Got %s", result)
+	}
+
+	if !HasTag(result, "bits", "100") {
+		t.Fatalf("Missing tag bits. Got %s", result)
+	}
+
+	if !HasCommand(result, "PRIVMSG") {
+		t.Fatalf("Missing command PRIVMSG. Got %s", result)
+	}
+}
+
+func TestMakeSubMessage(t *testing.T) {
+	result := MakeSubMessage(user, 1, channel)
+
+	if !HasTag(result, "msg-id", "sub") {
+		t.Fatalf("Missing tag msg-id. Got %s", result)
+	}
+
+	if !HasTag(result, "display-name", user) {
+		t.Fatalf("Missing tag display-name. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-cumulative-months", "1") {
+		t.Fatalf("Missing tag msg-param-cumulative-months. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-sub-plan", "Tier1") {
+		t.Fatalf("Missing tag msg-param-sub-plan. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-sub-plan-name", "Tier1") {
+		t.Fatalf("Missing tag msg-param-sub-plan-name. Got %s", result)
+	}
+
+	if !HasCommand(result, "USERNOTICE") {
+		t.Fatalf("Missing command USERNOTICE. Got %s", result)
+	}
+}
+
+func TestMakeReSubMessage(t *testing.T) {
+	result := MakeResubMessage(user, 1, channel)
+
+	if !HasTag(result, "msg-id", "resub") {
+		t.Fatalf("Missing tag msg-id. Got %s", result)
+	}
+
+	if !HasTag(result, "display-name", user) {
+		t.Fatalf("Missing tag display-name. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-cumulative-months", "1") {
+		t.Fatalf("Missing tag msg-param-cumulative-months. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-sub-plan", "Tier1") {
+		t.Fatalf("Missing tag msg-param-sub-plan. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-sub-plan-name", "Tier1") {
+		t.Fatalf("Missing tag msg-param-sub-plan-name. Got %s", result)
+	}
+
+	if !HasCommand(result, "USERNOTICE") {
+		t.Fatalf("Missing command USERNOTICE. Got %s", result)
+	}
+}
+
+func TestMakeGiftSubMessage(t *testing.T) {
+	result := MakeGiftSubMessage(user, "recipient", channel)
+
+	if !HasTag(result, "msg-id", "subgift") {
+		t.Fatalf("Missing tag msg-id. Got %s", result)
+	}
+
+	if !HasTag(result, "display-name", user) {
+		t.Fatalf("Missing tag display-name. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-recipient-display-name", "recipient") {
+		t.Fatalf("Missing tag msg-param-recipient-display-name. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-recipient-user-name", "recipient") {
+		t.Fatalf("Missing tag msg-param-recipient-user-name. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-gift-months", "1") {
+		t.Fatalf("Missing tag msg-param-gift-months. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-sub-plan", "Tier1") {
+		t.Fatalf("Missing tag msg-param-sub-plan. Got %s", result)
+	}
+
+	if !HasTag(result, "msg-param-sub-plan-name", "Tier1") {
+		t.Fatalf("Missing tag msg-param-sub-plan-name. Got %s", result)
 	}
 
 	if !HasCommand(result, "USERNOTICE") {
