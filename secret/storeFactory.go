37b45f68b2858529ee002cb786fca4d87552bdbf
Matthew Edge <MatthewEdge@users.noreply.github.com>
2021-01-30 08:33:00 -0500

    Remove vault as no longer required / used (#24)

--- secret/storeFactory.go
+++ secret/storeFactory.go
@@ -9,8 +9,7 @@ import (
 )
 
 const (
-	VAULT = "vault"
-	ENV   = "env"
+	ENV = "env"
 )
 
 // NewSecretStore returns a SecretStore for the given type, or an error if an invalid type is passed
@@ -21,17 +20,11 @@ func NewSecretStore(config config.Config) (Store, error) {
 	}
 
 	switch strings.ToLower(storeType) {
-	case VAULT:
-		vaultUrl := config.VaultAddress()
-		vaultToken := config.VaultToken()
-		store := NewVaultStore("secret/data/twitchToken")
-		err := store.Connect(vaultUrl, vaultToken)
-		return &store, err
 	case ENV:
 		twitchToken := config.TwitchToken()
 		store := NewEnvStore(twitchToken)
 		return &store, nil
 	default:
-		return nil, fmt.Errorf("Invalid storeType - %s. Valid values are: %s, %s", storeType, VAULT, ENV)
+		return nil, fmt.Errorf("Invalid storeType - %s. Valid values are: %v", storeType, []string{ENV})
 	}
 }
