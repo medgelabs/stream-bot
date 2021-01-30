bf081f2d817446c1661882f60472ffeae6872903
Matthew Edge <medge@medgelabs.io>
2021-01-30 08:02:00 -0500

    Fix missing tests. Add coverage report to action

--- bot/handlerTemplate.go
+++ bot/handlerTemplate.go
@@ -7,18 +7,18 @@ import (
 )
 
 type HandlerTemplate struct {
-	templ *template.Template
+	template *template.Template
 }
 
 func NewHandlerTemplate(t *template.Template) HandlerTemplate {
 	return HandlerTemplate{
-		templ: t,
+		template: t,
 	}
 }
 
 func (h HandlerTemplate) Parse(evt Event) string {
 	var msg strings.Builder
-	err := h.templ.Execute(&msg, evt)
+	err := h.template.Execute(&msg, evt)
 	if err != nil {
 		log.Printf("ERROR: bits template execute - %v", err)
 		return "" // We assume bot will not send empty messages
