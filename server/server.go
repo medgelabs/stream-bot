f7e3354c866c9e35b1e6ab0389e49b121cf5ee01
Matthew Edge <medge@medgelabs.io>
2021-07-17 09:20:30 -0400

    Fix metrics labels by moving template to embedded file

--- server/server.go
+++ server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"encoding/json"
+	"html/template"
 	"medgebot/bot"
 	"medgebot/cache"
+	"medgebot/logger"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,20 +13,27 @@ import (
 
 // Server REST API
 type Server struct {
-	router             *chi.Mux
-	viewerMetricsStore cache.Cache
-	alertWebSocket     *bot.WriteOnlyUnsafeWebSocket
-	debugClient        *DebugClient
+	router      *chi.Mux
+	bot         *bot.Bot
+	store       cache.Cache
+	debugClient *DebugClient
+	labelHTML   *template.Template
 }
 
 // New returns a Server instance to be run with http.ListenAndServe()
-func New(metricStore cache.Cache, alertWebSocket *bot.WriteOnlyUnsafeWebSocket, debugClient *DebugClient) *Server {
+func New(dataStore cache.Cache, debugClient *DebugClient, labelHTMLStr string) *Server {
 	srv := &Server{
-		router:             chi.NewRouter(),
-		viewerMetricsStore: metricStore,
-		alertWebSocket:     alertWebSocket,
-		debugClient:        debugClient,
+		router:      chi.NewRouter(),
+		store:       dataStore,
+		debugClient: debugClient,
 	}
+
+	// Parse Metrics Label HTML template for reuse by the View Handlers
+	tmpl, err := template.New("MetricsLabel").Parse(labelHTMLStr)
+	if err != nil {
+		logger.Fatal(err, "Failed to parse Metrics Label HTML template")
+	}
+	srv.labelHTML = tmpl
 
 	// TODO receive WebSocket connection for alerts and assign to alertWebSocket
 
@@ -35,6 +45,7 @@ func (s *Server) routes() {
 	// TODO pull from config
 	baseURL := "http://localhost:8080"
 
+	// Metrics endpoints
 	s.router.Get("/api/subs/last", s.fetchLastSub())
 	s.router.Get("/subs/last", s.lastSubView(baseURL+"/api/subs/last"))
 
@@ -44,11 +55,38 @@ func (s *Server) routes() {
 	s.router.Get("/api/bits/last", s.fetchLastBits())
 	s.router.Get("/bits/last", s.lastBitsView(baseURL+"/api/bits/last"))
 
+	// Polls
+	// s.router.Post("/polls", s.createPoll())
+
 	// DEBUG - trigger various events for testing
 	// TODO how do secure when deploy?
 	s.router.Get("/debug/sub", s.debugSub(s.debugClient))
 	s.router.Get("/debug/gift", s.debugGift(s.debugClient))
 	s.router.Get("/debug/bit", s.debugBit(s.debugClient))
+}
+
+// WriteJSON is a helper to respond with a JSON message body.
+// If marshalling fails, it will respond with a HTTP 500
+func (s *Server) WriteJSON(w http.ResponseWriter, statusCode int, msg interface{}) {
+	w.WriteHeader(statusCode)
+	w.Header().Add("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(msg)
+	if err != nil {
+		logger.Error(err, "Failed to write Error response")
+		s.WriteError(w, 500, "Response marshalling failed")
+	}
+}
+
+// Standard errors
+type error struct {
+	Error string `json:"error"`
+}
+
+// WriteError responds with a standardized error body
+func (s *Server) WriteError(w http.ResponseWriter, statusCode int, msg string) {
+	s.WriteJSON(w, statusCode, error{
+		Error: msg,
+	})
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
